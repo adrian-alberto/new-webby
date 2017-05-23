14b863561a60219fd37641f21105ef12c68f0c62
Adrian Alberto <woftles@gmail.com>
2017-05-23 15:06:30 -0700

    Init

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE=HTML>
+<html>
+
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700|Source+Code+Pro" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/css/index.css">
+</head>
+
+
+
+
+
+<body>
+	<div class="center">
+		<div id="navpanel">
+			<a href="/" id="aboutlink">about</a><br>
+			<a href="/">cv</a><br>
+			<a href="https://github.com/woftles">github</a><br>
+			<a href="mailto:adrian@whitecollargames.com">e-mail</a><br>
+		</div>
+
+		<div id="about">
+			<p>
+				i am adrian alberto<br>
+				<span class="invisitext">i am</span> a <a href="/">research assistant</a><br>
+				<span class="invisitext">i am</span> a <a href="/">game developer</a><br>
+				<span class="invisitext">i am</span> an <a href="/">artist</a><br>
+				<span class="invisitext">i am</span> a <a href="/">programmer</a><br>
+				<span class="invisitext">don't tell anybody but i'm not a web developer</span>
+			</p>
+		</div>
+
+		<hr class="clear">
+	</div>
+</body>
+
+</html>
