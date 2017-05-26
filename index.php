4dc38ac1b3938bf5e55d0eee2c4ce3427452b215
Adrian Alberto <woftles@gmail.com>
2017-05-25 19:14:37 -0700

    Added portfolio grid

--- index.php
+++ index.php
@@ -32,6 +32,66 @@
 		</div>
 
 		<hr class="clear">
+
+		<div id="portfolio">
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+
+<?php
+$dir = $_SERVER['DOCUMENT_ROOT']."/cms/";
+$folders = scandir($dir);
+
+$thumbs = array();
+$ratios = array();
+foreach ($folders as $i => $folder) {
+	#echo $folder . "<br>";
+	$thumbname = "thumb_$folder.png";
+	$thumbpath = "$dir$folder/$thumbname";
+	if (is_file($thumbpath)) {
+		#echo $thumbpath . "<br>";
+		$size = getimagesize($dir.$folder."/".$thumbname);
+		$w = $size[0];
+		$h = $size[1];
+		array_push($thumbs, "/cms/$folder/$thumbname");
+		array_push($ratios, $w/$h);
+	}
+}
+
+function makeButton($link, $img, $wPercent, $numRow) {
+	$mg = ($numRow-1)*4; #margin
+	echo "<a><img src=\"$img\" style=\"width:$wPercent%;\"></a>";
+}
+
+
+$totalRatio = 0.0;
+$maxRatio = 5.0;
+$currentRow = array();
+$currentRowRatios = array();
+foreach ($thumbs as $i => $thumbpath) {
+	$ratio = $ratios[$i];
+	if ($totalRatio + $ratio > $maxRatio) {
+		echo "<div class=\"portfoliorow\">";
+		foreach ($currentRow as $j => $tp) {
+			$rj = $currentRowRatios[$j];
+			makeButton("/",$tp, 100.0*$rj/$totalRatio, count($currentRow)) ;	
+		}
+		echo "</div>";
+		$totalRatio = 0.0;
+		$currentRow = array();
+		$currentRowRatios = array();
+		
+	}
+
+	#echo "$thumbpath ";
+	$totalRatio = $totalRatio + $ratio;
+	array_push($currentRow, $thumbpath);
+	array_push($currentRowRatios, $ratio);
+}
+
+
+?>
+
+<!-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
+		</div>
 	</div>
 </body>
 
